6a4b2a5e965b3d4c270623f9b99a9affab16e6a1
torcado <torcado@yahoo.com>
2019-01-08 22:29:28 -0700

    more stuff

--- index.js
+++ index.js
@@ -4,26 +4,51 @@ const Zlib = require('zlib');
 
 var Disco = {
     VERSION: "0.0.1",
+    GATEWAY_VERSION: 6,
     clients: []
 };
 
 Disco.Client = function(token){
     let client = this;
-    Disco.clients.push(this);
     Emitter.call(this);
+    Disco.clients.push(this);
     
     client.guilds = [];
     
-    client._token = token;
-    client.connecting = true;
+    client.connecting = false;
     client.ready = false;
-    client._socket = new WebSocket(gateway);
-    
+    client.pings = [];
+    client._token = token;
     client._seq = null;
     
-    client._socket.once('close', hSocketClose);
-    client._socket.once('error', hSocketClose);
-    client._socket.on('message', hSocketMessage);
+    client.presence = {
+        game: {
+            name: "",
+            type: 0
+        },
+        status: "online",
+        since: Date.now(),
+        afk: false
+    }
+    
+    
+    function connect(){
+        get(Disco.Endpoints.GATEWAY, (err, res) => {
+            if(err){
+                console.error(err, res);
+                client.emit('error', err, res);
+            } else {
+                client._gatewayURL = res.body.url + "/?encoding=json&v=" + Disco.GATEWAY_VERSION;
+                client._socket = new WebSocket(client._gatewayURL);
+                client.connecting = true;
+                
+                client._socket.once('close', hSocketClose);
+                client._socket.once('error', hSocketClose);
+                client._socket.on('message', hSocketMessage);
+            }
+        });
+    }
+    connect();
     
     function hSocketClose(code, data){
         client.connecting = false;
@@ -38,24 +63,24 @@ Disco.Client = function(token){
             case 0: //Dispatch | Receive | dispatches an event
                 client._seq = message.s;
                 break;
-            case: 1 //Heartbeat | Send/Receive | used for ping checking
+            case 1: //Heartbeat | Send/Receive | used for ping checking
                 send(Disco.Payloads.HEARTBEAT(client));
                 break;
-            case: 2 //Identify | Send | used for client handshake
+            case 2: //Identify | Send | used for client handshake
                 break;
-            case: 3 //Status Update | Send | used to update the client status
+            case 3: //Status Update | Send | used to update the client status
                 break;
-            case: 4 //Voice State Update | Send | used to join/move/leave voice channels
+            case 4: //Voice State Update | Send | used to join/move/leave voice channels
                 break;
-            case: 6 //Resume | Send | used to resume a closed connection
+            case 6: //Resume | Send | used to resume a closed connection
                 break;
-            case: 7 //Reconnect | Receive | used to tell clients to reconnect to the gateway
+            case 7: //Reconnect | Receive | used to tell clients to reconnect to the gateway
                 clearTimeout(client._heartbeat);
                 client._socket.close(1000, 'Reconnect requested by Discord');
                 break;
-            case: 8 //Request Guild Members | Send | used to request guild members
+            case 8: //Request Guild Members | Send | used to request guild members
                 break;
-            case: 9 //Invalid Session | Receive | used to notify client they have an invalid session id
+            case 9: //Invalid Session | Receive | used to notify client they have an invalid session id
                 if(message.d){
                     idResume(client);
                 } else {
@@ -66,10 +91,42 @@ Disco.Client = function(token){
                     }, Math.random()*4000+1000);
                 }
                 break;
-            case: 10 //Hello | Receive | sent immediately after connecting, contains heartbeat and server debug information
+            case 10: //Hello | Receive | sent immediately after connecting, contains heartbeat and server debug information
+                client._hbIntvl = message.heartbeat_interval;
+                client._hb = setInterval(heartbeat, client._hbIntvl);
                 break;
-            case: 11 //Heartbeat ACK | Receive | sent immediately following a client heartbeat that was received
+            case 11: //Heartbeat ACK | Receive | sent immediately following a client heartbeat that was received
+                client._hbAckd
+                client.pings.unshift(Date.now() - client._lastHeartbeat);
+                if(client.pings.length > 10){
+                    client.pings.pop();
+                }
                 break;
+        }
+        
+        if(message.op === 0){
+            let event = message.t;
+            
+            switch(event){
+                case "READY":
+                    client.user = message.d.user;
+                    client.guilds = message.d.guilds;
+                    client._sessionID = message.d.session_id;
+                    
+                    client.ready = true;
+                    client.connecting = false;
+                    break;
+            }
+        }
+    }
+    
+    function heartbeat(){
+        client._lastHb = Date.now();
+        if(client._hbAckd){
+            client._hbAckd = false;
+            send(client._socket, Disco.Payloads.HEARTBEAT(client));
+        } else {
+            client._socket.close(1001, "No heartbeat received");
         }
     }
     
@@ -78,17 +135,52 @@ Disco.Client = function(token){
         return flags.binary ? JSON.parse(Zlib.inflateSync(data).toString()) : JSON.parse(data);
     }
     
+    function idResume(){
+        if(client._seq && client._token && client._sessionID){
+            send(Disco.Payloads.RESUME(client));
+        } else {
+            send(Disco.Payloads.IDENTIFY(client));
+        }
+    }
+    
     function send(){
         if(client._socket && client._socket.readyState == 1)
         client._socket.send(JSON.stringify(data));
     }
 }
 
+{
+    let API = "https://discordapp.com/api",
+        CDN = "https://cdn.discordapp.com",
+        ME = API + "/users/@me";
+    
+    Disco.Endpoints = {
+        API,
+        CDN,
+        ME,
+        GATEWAY: API + "/gateway"
+    }
+}
 
 Disco.Payloads = {
-    HEARTBEAT: function(client) {
+    IDENTIFY: client => ({
+        token: client._token,
+        properties: {
+            $os: navigator.platform,
+            $browser: "disco",
+            $device: "disco"
+        },
+        compress: true,
+        large_threshold: LARGE_THRESHOLD,
+        presence: client._presence
+    }),
+    RESUME: client => ({
+        op: 6, 
+        d: client._seq
+    }),
+    HEARTBEAT: client => ({
         return {op: 1, d: client._seq};
-    },
+    }),
 }
 
 
